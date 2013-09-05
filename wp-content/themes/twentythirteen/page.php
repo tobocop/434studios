41b617158fe920e4850bafb38a619720947e8376
Toby Rumans <toby@plink.com>
2013-09-04 20:15:28 -0600

    maintenance page

--- wp-content/themes/twentythirteen/page.php
+++ wp-content/themes/twentythirteen/page.php
@@ -12,7 +12,6 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
---
+++ wp-content/wp-maintenance-mode.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>434studios - Coming soon!</title>
+	<style>
+body{
+font-family: 'Helvetica', 'Arial';
+}
+.content{
+width:400px;
+height:250px;
+position:absolute;
+background:white;
+left:50%;
+top:100px;
+margin-left:-200px;
+-webkit-border-radius: 10px;
+-moz-border-radius: 10px;
+border-radius: 10px;
+}
+
+h1 {
+margin:0;
+padding:5px;
+color:#83C340;
+}
+
+a {
+color:#83C340;
+text-decoration:none;
+}
+	</style>
+  </head>
+  <body style="background:#83C340;position:relative;">
+	<div class="content">
+		<h1>434Studios</h1>
+<p style="padding:5px 10px 0px 20px;">
+	Our site will be up and running awesome things shortly!
+</p>
+<p style="padding:5px 10px 0px 20px;">
+If you have any questions about web development work, please e-mail me at <a href="mailto:toby@434studios.com">toby@434studios.com</a>
+</p>
+	</div>
+  </body>
+</html>
